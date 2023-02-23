b2d5124552151a1346a8761f573a37d5f5847b6a
Jim Martens <jma@hbt.de>
2023-02-23 13:56:28 +0100

    Modified pipeline to push docker image

--- .github/workflows/scripts/build_and_push_dev.sh
+++ .github/workflows/scripts/build_and_push_dev.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e
 
-DOCKER_IMAGE="hbt/digitransit-ui"
 DOCKER_TAG=${DOCKER_BASE_TAG:-latest}
 
 COMMIT_HASH=$(git rev-parse --short "$GITHUB_SHA")
 
 DOCKER_TAG_LONG=$DOCKER_TAG-$(date +"%Y-%m-%dT%H.%M.%S")-$COMMIT_HASH
-DOCKER_IMAGE_TAG=$DOCKER_IMAGE:$DOCKER_TAG
-DOCKER_IMAGE_TAG_LONG=$DOCKER_IMAGE:$DOCKER_TAG_LONG
+DOCKER_IMAGE_TAG=$ECR_REGISTRY/$ECR_REPOSITORY:$DOCKER_TAG
+DOCKER_IMAGE_TAG_LONG=$ECR_REGISTRY/$ECR_REPOSITORY:$DOCKER_TAG_LONG
 
 # Build image
 echo "Building digitransit-ui"
 echo -e "export const COMMIT_ID = \"${GITHUB_SHA}\";\nexport const BUILD_TIME = \""`date -Iminutes -u`"\";" > app/buildInfo.js
 docker build --tag=$DOCKER_IMAGE_TAG_LONG .
 
-docker login -u $DOCKER_USER -p $DOCKER_AUTH
 echo "Pushing $DOCKER_TAG image"
 docker push $DOCKER_IMAGE_TAG_LONG
 docker tag $DOCKER_IMAGE_TAG_LONG $DOCKER_IMAGE_TAG
